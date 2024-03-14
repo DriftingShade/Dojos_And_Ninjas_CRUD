6b95f036b91d05b6c28686f0ff204ef69082fecb
DriftingShade <sarus333@yahoo.com>
2024-03-13 23:15:49 -0600

    More Progress! Added Ninjas!

--- flask_app/controllers/dojos.py
+++ flask_app/controllers/dojos.py
@@ -1,16 +1,28 @@
 from flask_app import app
 from flask import render_template, redirect, request
 from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
+
 
 @app.route("/")
 def index():
     all_dojos = Dojo.get_all()
     return render_template("dojos.html", all_dojos=all_dojos)
 
+
 @app.post("/create_dojo")
 def create_dojo():
-    data = {
-        "name": request.form["name"]
-    }
+    data = {"name": request.form["name"]}
     Dojo.save(data)
     return redirect("/")
+
+
+@app.route("/dojo_info/<int:dojo_id>")
+def get_ninjas(dojo_id):
+    ninjas = Ninja.get_dojo_ninjas({"dojo_id": dojo_id})
+    return render_template("dojo_show.html", ninjas=ninjas)
+
+
+@app.route("/ninja_page")
+def ninja_page():
+    return render_template("new_ninja.html")
---
+++ flask_app/models/ninja.py
@@ -0,0 +1,32 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+
+class Ninja:
+    DB = "dojos_and_ninjas_schema"
+
+    def __init__(self, data):
+        self.id = data["id"]
+        self.dojo_id = data["dojo_id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.age = data["age"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+    @classmethod
+    def get_dojo_ninjas(cls, data):
+        query = "SELECT * FROM ninjas WHERE dojo_id = %(dojo_id)s;"
+        print(query)
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print(results)
+        ninjas = []
+        for ninja in results:
+            ninjas.append(cls(ninja))
+        return ninjas
+
+    @classmethod
+    def save(cls, data):
+        query = """INSERT INTO ninjas (%(dojo_id)s, first_name, last_name, age, created_at, 
+        updated_at) VALUES (%(dojo_id)s, %(first_name)s, %(last_name)s, %(age)s, NOW(), NOW());"""
+
+        return connectToMySQL(cls.DB).query_db(query, data)
